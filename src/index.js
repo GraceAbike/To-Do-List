86de883692ebd68a1ffbf8f43b9ba7d8988f8c19
Grace Abike <mara234216@gmail.com>
2022-10-06 13:10:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './style.css';
 import addTask from './modules/addTask.js';
 import renderToDoItems from './modules/renderTasks.js';
+import completedClear from './modules/completedClear.js';
+import allClear from './modules/allClear.js';
+import indexInit from './modules/indexInit.js';
 
 addTask();
 renderToDoItems();
+completedClear();
+allClear();
+indexInit();

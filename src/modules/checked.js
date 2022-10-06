435e956a6f5e1dda05767b45a20fc97a67236ffc
Grace Abike <mara234216@gmail.com>
2022-10-06 13:10:52 +0200

    Create checked.js

---
+++ src/modules/checked.js
@@ -0,0 +1,23 @@
+import itemsArray from './itemsArray.js';
+
+const checkedBoxEls = document.getElementsByClassName('checkbox');
+
+const checked = () => {
+  const checkedBoxElsArray = [...checkedBoxEls];
+  checkedBoxElsArray.forEach((item) => {
+    item.addEventListener('click', () => {
+      const itmId = item.parentNode.parentNode.id;
+      const { completed } = itemsArray[itmId];
+
+      if (completed) {
+        itemsArray[itmId].completed = false;
+      } else {
+        itemsArray[itmId].completed = true;
+      }
+
+      localStorage.setItem('itemsArray', JSON.stringify(itemsArray));
+    });
+  });
+};
+
+export default checked;

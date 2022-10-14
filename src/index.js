376367873ae1dbc100fcd5b649a48b5d0f3298f9
Grace Abike <mara234216@gmail.com>
2022-10-14 11:23:23 +0200

    Add add Task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import './style.css';
+import './styles.css';
 import addTask from './modules/addTask.js';
+import itemsArray from './modules/itemsArray.js';
+import removeTast from './modules/remove.js';
 import renderToDoItems from './modules/renderTasks.js';
 import completedClear from './modules/completedClear.js';
 import allClear from './modules/allClear.js';
 import indexInit from './modules/indexInit.js';
 
-addTask();
+const todoinputEl = document.querySelector('.to-do-input');
+const addForm = document.querySelector('.to-do-add');
+
+const updateTasks = () => {
+  localStorage.setItem('toDoItemsArray', JSON.stringify(itemsArray));
+  renderToDoItems();
+};
+
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addTask(todoinputEl.value);
+  updateTasks();
+  todoinputEl.value = '';
+  indexInit();
+});
+
 renderToDoItems();
 completedClear();
 allClear();
-indexInit();
+
+const removeBtnsEl = [...document.getElementsByClassName('dltTaskBtn')];
+removeBtnsEl.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    removeTast(e.target.id);
+    updateTasks();
+  });
+});
--- src/modules/addTask.js
+++ src/modules/addTask.js
@@ -1,33 +1,20 @@
 import itemsArray from './itemsArray.js';
-import renderToDoItems from './renderTasks.js';
-import indexInit from './indexInit.js';
 
-const addForm = document.querySelector('.to-do-add');
-const input = document.querySelector('input');
-const todoinputEl = document.querySelector('.to-do-input');
-
-const addTask = () => {
-  addForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const newTask = todoinputEl.value;
-    if (newTask) {
-      const newTodo = {
-        description: newTask,
-        completed: false,
-        index: itemsArray.length + 1,
-      };
-      itemsArray.push(newTodo);
-      localStorage.setItem('itemsArray', JSON.stringify(itemsArray));
-      renderToDoItems();
-      indexInit();
-      todoinputEl.value = '';
-    } else {
-      input.classList.add('inputError');
-      setTimeout(() => {
-        input.classList.remove('inputError');
-      }, 1000);
-    }
-  });
+const addTask = (newTask) => {
+  const input = document.querySelector('input');
+  if (newTask) {
+    const newTodo = {
+      description: newTask,
+      completed: false,
+      index: itemsArray.length + 1,
+    };
+    itemsArray.push(newTodo);
+  } else {
+    input.classList.add('inputError');
+    setTimeout(() => {
+      input.classList.remove('inputError');
+    }, 1000);
+  }
 };
 
 export default addTask;

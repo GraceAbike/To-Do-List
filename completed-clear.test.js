66fffcd35c606813cb557c02c101022850159484
unknown <ekpenisiraphael@gmail.com>
2022-10-14 20:46:57 +0530

    Clear and edited tested

--- completed-clear.test.js
+++ completed-clear.test.js
@@ -4,3 +4,22 @@ import editTask from './src/modules/edit.js';
 import completedClear from './src/modules/completedClear.js';
 import completed from './src/modules/completedUpdate.js';
 
+describe('test edit and clear completed', () => {
+  test('test edit task function', () => {
+    addTask('First task');
+    addTask('Second task');
+    editTask('changed', 0);
+    expect(itemsArray[0].description).toBe('changed');
+  });
+
+  test('test completed function', () => {
+    completed(0, true);
+    expect(itemsArray[0].description).toBe(true);
+  });
+
+  test('test clear completed task function', () => {
+    itemsArray[0].completed = true;
+    completedClear();
+    expect(itemsArray).toHaveLength(1);
+  });
+});

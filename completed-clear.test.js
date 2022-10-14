d43793c6d025fa436bcfe55e7ea4f9bc65222e43
Grace Abike <mara234216@gmail.com>
2022-10-14 17:09:01 +0200

    Create completed-clear.test.js

---
+++ completed-clear.test.js
@@ -0,0 +1,6 @@
+import addTask from './src/modules/addTask.js';
+import itemsArray from './src/modules/itemsArray.js';
+import editTask from './src/modules/edit.js';
+import completedClear from './src/modules/completedClear.js';
+import completed from './src/modules/completedUpdate.js';
+

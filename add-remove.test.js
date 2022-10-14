d1caea1c7bcb827c56f24170b0dfbd0e1a20cab1
unknown <ekpenisiraphael@gmail.com>
2022-10-14 15:18:21 +0530

    add and remove function tested with jest

--- add-remove.test.js
+++ add-remove.test.js
@@ -2,3 +2,15 @@ import addTask from './src/modules/addTask.js';
 import itemsArray from './src/modules/itemsArray.js';
 import removeT from './src/modules/remove.js';
 
+describe('test addTask and removeTask', () => {
+  test('test add task function', () => {
+    addTask('first task');
+    addTask('second task');
+    expect(itemsArray).toHaveLength(2);
+  });
+
+  test('test remove task function', () => {
+    removeT(0);
+    expect(itemsArray).toHaveLength(1);
+  });
+});
